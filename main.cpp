1183adf7d00aba1bbaf9088bb4ba54f32ae6f7f2
Valeria <timohinava2005@gmail.com>
2024-10-24 21:42:10 +0700

    Нужно добавить комментарии и почистить main

--- main.cpp
+++ main.cpp
@@ -3,26 +3,31 @@
 #include <cstddef>
 #include <map>
 
-template <class T>
+template <class T, size_t poolSize = 10>
 struct CustomAllocator {
+private:
+    size_t currentMemory; //текущий размер блока
+    size_t allocatedElements; //количество выделенных элементов
+    T * memory; //указатель на начало блока
+public:
     using value_type = T;
     void* pool;
     // Конструктор по умолчанию инициализирует CustomAllocator (не выбрасывает исключения)
-    CustomAllocator () noexcept {
-        pool = ::operator new(10*sizeof(T));
-    } 
+    CustomAllocator () : currentMemory(poolSize), allocatedElements(0), memory(static_cast<T*>(std::malloc(poolSize * sizeof(T)))) {
+        if (!memory) {
+            throw std::bad_alloc();  // Если выделение памяти не удалось, выбрасываем исключение std::bad_alloc
+        }      
+    }
     // Деструктор освобождает всю ранее выделенную память при уничтожении объекта CustomAllocator
-    ~CustomAllocator() { ::operator delete(pool); }
+    ~CustomAllocator() { std::free(memory); }
     // Конструктор копирования, который позволяет копировать из другого CustomAllocator другого типа U (не выбрасывает исключения)
     template <class U> CustomAllocator (const CustomAllocator<U>&) noexcept {}
     // Функция выделения памяти для n объектов типа T
     T* allocate (std::size_t n) {
-        return static_cast<T*>(::operator new((2*n*sizeof(T) + 1))); // используется глобальный operator new
+        return static_cast<T*>(::operator new(n*sizeof(T))); // используется глобальный operator new
     }
     // Функция освобождения памяти, на которую указывает p
-    void deallocate (T* p, std::size_t n) { 
-        ::operator delete(p); // используется глобальный operator delete
-    } 
+    void deallocate (T* p, std::size_t n) { ::operator delete(p); } // используется глобальный operator delete 
     // Структура, которая позволяет повторно связывать аллокатор с другим типом U
     template< class U >
     struct rebind {
@@ -30,14 +35,60 @@ struct CustomAllocator {
     };
 };
 
+// Оператор == для сравнения аллокаторов (всегда возвращает true)
 template <class T, class U>
 constexpr bool operator== (const CustomAllocator<T>& a1, const CustomAllocator<U>& a2) noexcept {
     return true;
 }
+// Оператор != для сравнения аллокаторов (всегда возвращает false)
 template <class T, class U>
 constexpr bool operator!= (const CustomAllocator<T>& a1, const CustomAllocator<U>& a2) noexcept {
     return false;
 }
+
+// Класс ограниченного контейнера (с фиксированным размером)
+template <typename T, size_t maxSize, typename Allocator = CustomAllocator<T>>
+class LimitedContainer {
+private:
+    Allocator alloc; // экземпляр аллокатора
+    T* data; // указатель на данные
+    size_t size; // текущий размер контейнера
+public:
+    // Конструктор по умолчанию (инициализирует данные, размер и аллокатор)
+    LimitedContainer() : alloc(), data(nullptr), size(0) {}
+    // Деструктор
+    ~LimitedContainer() {
+        if (data) {
+            alloc.deallocate(data, size);
+        }
+    }
+    // Добавление элемента в конец контейнера, если контейнер заполнен, выбрасывается исключение
+    void push_back(const T& value) {
+        if (size == maxSize) { // проверка на заполненность контейнера
+            throw std::runtime_error("Контейнер уже заполнен!"); 
+        }
+        if (size == 0) { // если контейнер пустой, выделяем память для данных
+            data = alloc.allocate(maxSize); 
+        }
+        new (&data[size]) T(value); // использование placement new для создания нового элемента типа T
+        ++size; // увеличение размера контейнера
+    }
+    // Возвращение ссылки на элемент по заданному индексу
+    const T& operator[](size_t index) const {
+        if (index >= size) {
+            throw std::out_of_range("Неверный индекс!");
+        }
+        return data[index];
+    }
+    // Возвращение текущего размера контейнера
+    size_t getSize() const {
+        return size;
+    }
+    // Проверка, пуст ли контейнер
+    bool empty() const {
+        return size == 0;
+    }
+};
 
 template <typename T, typename Allocator = CustomAllocator<T>>
 class BidirectionalList {
@@ -50,7 +101,7 @@ private:
     };
     Node* head;
     Node* tail;
-    int numberOfElements;
+    size_t numberOfElements;
     Allocator allocator;
 public:
     struct Iterator {
@@ -94,6 +145,7 @@ public:
         other.tail = nullptr;
         other.numberOfElements = 0;
     }
+    ~BidirectionalList() { clear(); }
     void push_back(const T& value) {
         typename Allocator::template rebind<Node>::other nodeAlloc;
         Node* newNode = nodeAlloc.allocate(1);
@@ -124,16 +176,15 @@ public:
         }
         return false;
     }
-    int get_size() const {
+    size_t get_size() const {
         return numberOfElements;
     }
-    ~BidirectionalList() {
+    // Очистка контейнера
+    void clear() {
         Node* current = head;
         while (current != nullptr) {
             Node* next = current->next;
-            // Используем аллокатор для освобождения памяти
-            typename Allocator::template rebind<Node>::other nodeAlloc;
-            nodeAlloc.deallocate(current, 1);
+            delete current;
             current = next;
         }
     }
@@ -152,7 +203,7 @@ int main() {
         standardMap[i] = factorial(i);
     }
     // создание экземпляра std::map<int, int> с новым аллокатором, ограниченным 10 элементами
-    std::map<int, int, std::less<int>, CustomAllocator<std::pair<const int, int>>> allocatorMap;
+    std::map<int, int, std::less<int>, CustomAllocator<std::pair<const int, int>, 10>> allocatorMap;
     // заполнение 10 элементами, где ключ – это число от 0 до 9, а значение – факториал ключа
     for (int i = 0; i < 10; ++i) {
         allocatorMap[i] = factorial(i);
@@ -175,5 +226,14 @@ int main() {
     }
     // вывод на экран всех значений, хранящихся в контейнере
     allocatorList.print();
+
+    LimitedContainer<std::pair<const int, int>, 10, CustomAllocator<std::pair<const int, int>, 10>> limited_map;
+    for (int i = 0; i < 10; ++i) {
+        limited_map.push_back(std::make_pair(i, factorial(i)));
+    }
+    std::cout << "LimitedContainer: ";
+    for (size_t i = 0; i < limited_map.getSize(); ++i) {
+        std::cout << limited_map[i].first << " " << limited_map[i].second << std::endl;
+    }
     return 0;
 }
